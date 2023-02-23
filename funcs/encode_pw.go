6bc2eaf61e344b37c507018cc837ccaa850f6d92
hui <1577108595@qq.com>
2023-02-23 21:32:46 +0800

    fix:bug

--- funcs/encode_pw.go
+++ funcs/encode_pw.go
@@ -2,6 +2,7 @@ package funcs
 
 import "golang.org/x/crypto/bcrypt"
 
+// 加密密码
 func Encrypthon_PW(Old_PW string) (New_PW string) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(Old_PW), bcrypt.DefaultCost)
 	if err != nil {
